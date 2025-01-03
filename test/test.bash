bc286a25cdfc5f2d51d7ce573230a551328aea9a
Reo Isaka <s23c1009ge@s.chibakoudai.jp>
2025-01-03 17:43:49 +0900

    ADD test

--- test/test.bash
+++ test/test.bash
@@ -8,7 +8,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 63 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 70 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'Received: 0:01:03'
