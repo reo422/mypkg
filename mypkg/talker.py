a1e93f28b8767dca10db355e0a81b78c261e04fb
Reo Isaka <s23c1009ge@s.chibakoudai.jp>
2025-01-03 17:04:10 +0900

    count

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,21 +1,32 @@
+# SPDX-FileCopyrightText: 2024 Reo Isaka
+# SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.msg import String
+
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
+pub = node.create_publisher(String, "countup", 10)
+counter = 0
 
 
 def cb():
-    global n
-    msg = Int16()
-    msg.data = n
+    global counter
+
+    hours = counter // 3600
+    minutes = (counter % 3600)  // 60
+    seconds = counter % 60
+    time_str = f"{hours:01}:{minutes:02}:{seconds:02}"
+
+
+    msg = String()
+    msg.data = time_str
     pub.publish(msg)
-    n += 1
+    counter += 1
 
 
 def main():
-    node.create_timer(0.5, cb)
+    node.create_timer(1.0, cb)
     rclpy.spin(node)
