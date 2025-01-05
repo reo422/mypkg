6c7bdc7356c9e61a854358957c5d4f04a28a4d45
Reo Isaka <s23c1009ge@s.chibakoudai.jp>
2025-01-05 15:47:20 +0900

    Alert

---
+++ mypkg/timepublisher.py
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: 2024 Reo Isaka
+# SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+rclpy.init()
+node = Node("timepublisher")
+pub = node.create_publisher(String, "timenotifications", 10)
+counter = 0
+
+
+def cb():
+    global counter
+
+    if counter == 10:
+        alert = String()
+        alert.data = "10 seconds have passed!"
+        pub.publish(alert)
+
+    if counter == 60:
+        secondalert = String()
+        secondalert.data = "1 minute has passed!"
+        pub.publish(secondalert)
+
+    counter += 1
+
+
+def main():
+    node.create_timer(1.0, cb)
+    rclpy.spin(node)
