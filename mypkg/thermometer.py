595242857013ba515fe50de4f2aba438cb4d4a14
Reo Isaka <s23c1009ge@s.chibakoudai.jp>
2025-01-05 23:11:13 +0900

    alert

--- mypkg/thermometer.py
+++ mypkg/thermometer.py
@@ -18,8 +18,7 @@ def cb():
     if counter < len(testbt):
         bt = testbt[counter]
         alert = String()
-        alert.data = f"{bt:.1f}"
-        node.get_logger().info(f"動作確認:{alert.data}")
+        alert.data = f"動作確認:{bt:.1f}"
         pub.publish(alert)
         counter += 1
     else:
