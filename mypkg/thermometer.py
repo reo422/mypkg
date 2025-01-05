cedf324ec02d95d91d7990a13de800ca90235d91
Reo Isaka <s23c1009ge@s.chibakoudai.jp>
2025-01-05 22:26:55 +0900

    alert

--- mypkg/thermometer.py
+++ mypkg/thermometer.py
@@ -10,12 +10,23 @@ rclpy.init()
 node = Node("temperaturepublisher")
 pub = node.create_publisher(String, "temperaturealerts", 10)
 
+testbt = [35.8, 36.5, 38.0]
+counter = 0
 
 def cb():
-    bt = random.uniform(35.5, 42.0)
-    alert = String()
-    alert.data = f"{bt:.1f}"
-    pub.publish(alert)
+    global counter
+    if counter < len(testbt):
+        bt = testbt[counter]
+        alert = String()
+        alert.data = f"{bt:.1f}"
+        node.get_logger().info(f"動作確認:{alert.data}")
+        pub.publish(alert)
+        counter += 1
+    else:
+        bt = random.uniform(35.5, 42.0)
+        alert = String()
+        alert.data = f"{bt:.1f}"
+        pub.publish(alert)
 
 def main():
     node.create_timer(1.0, cb)
