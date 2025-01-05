4ff1a415681372b5b515458c983140aae582a575
Reo Isaka <s23c1009ge@s.chibakoudai.jp>
2025-01-05 20:16:35 +0900

    launch

--- launch/temp.launch.py
+++ launch/temp.launch.py
@@ -6,14 +6,14 @@ import launch_ros.actions
 
 def generate_launch_description():
     
-     timepublisher = launch_ros.actions.Node(
+     thermometer = launch_ros.actions.Node(
          package='mypkg',      #パッケージの名前を指定
-         executable='timepublisher'   #実行するファイルの指定
+         executable='thermometer'   #実行するファイルの指定
          )
-     timealert = launch_ros.actions.Node(
+     bodyalert = launch_ros.actions.Node(
          package='mypkg',
-         executable='timealert',
+         executable='bodyalert',
          output='screen'        #ログを端末に出すための設定
          )
 
-     return launch.LaunchDescription([timepublisher, timealert])
+     return launch.LaunchDescription([thermometer, bodyalert])
