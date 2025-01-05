7e6effaac8ff95bbc6da1d51c6ab037ab6f4da05
Reo Isaka <s23c1009ge@s.chibakoudai.jp>
2025-01-05 16:44:34 +0900

    launch

--- launch/time.launch.py
+++ launch/time.launch.py
@@ -6,14 +6,14 @@ import launch_ros.actions
 
 def generate_launch_description():
     
-     talker = launch_ros.actions.Node(
+     timepublisher = launch_ros.actions.Node(
          package='mypkg',      #パッケージの名前を指定
          executable='timepublisher'   #実行するファイルの指定
          )
-     listener = launch_ros.actions.Node(
+     timealert = launch_ros.actions.Node(
          package='mypkg',
-         executable='alert',
+         executable='timealert',
          output='screen'        #ログを端末に出すための設定
          )
 
-     return launch.LaunchDescription([timepublisher, alert])
+     return launch.LaunchDescription([timepublisher, timealert])
