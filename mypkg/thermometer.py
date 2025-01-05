39d0956d77200531d70220a5a93d72417c2ac3b0
Reo Isaka <s23c1009ge@s.chibakoudai.jp>
2025-01-05 19:57:19 +0900

    taion

--- mypkg/thermometer.py
+++ mypkg/thermometer.py
@@ -4,29 +4,27 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-
+import random
 
 rclpy.init()
-node = Node("timepublisher")
-pub = node.create_publisher(String, "timenotifications", 10)
-counter = 0
+node = Node("temperaturepublisher")
+pub = node.create_publisher(String, "temperaturealerts", 10)
 
 
 def cb():
-    global counter
+    bt = random.uniform(35.5, 42.0)
+    alert = String()
 
-    if counter == 10:
-        alert = String()
-        alert.data = "10秒経過しました! システムは正常に動いてます。"
-        pub.publish(alert)
-
-    if counter == 60:
-        secondalert = String()
-        secondalert.data = "1分が経過しました!システムはまだ実行中です。"
-        pub.publish(secondalert)
-
-    counter += 1
-
+    if bt < 36.0: 
+        alert.data = f"{bt:.2f}度。体を温めてください。"
+    elif 36.0 <= bt < 37.0:
+        alert.data = f"{bt:.2f}度。正常です。"
+    elif 37.0 <= bt < 38.0:
+        alert.data = f"{bt:.2f}度。安静が必要です。"
+    else:
+        alert.data = f"{bt:.2f}度。すぐ医師に相談してください。"
+ 
+    pub.publish(alert)
 
 def main():
     node.create_timer(1.0, cb)
